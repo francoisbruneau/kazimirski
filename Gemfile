ccebb544e106f9c0b2e383d82e61b3edda074028
François Bruneau <francois.bruneau@liip.ch>
2015-01-02 17:56:07 +0100

    basic Devise & Simple Form setup

--- Gemfile
+++ Gemfile
@@ -11,6 +11,9 @@ gem 'sass-rails', '~> 4.0'
 
 gem 'bootstrap-sass', '~> 3.3.1'
 
+gem 'simple_form'
+gem 'devise'
+
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,11 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
+  config.action_mailer.delivery_method = :smtp
+  # TODO: Move passwords away from version control
+  config.action_mailer.smtp_settings = { address: 'smtp.mandrillapp.com', port: 587, user_name: 'francois.bruneau49@gmail.com', password: Rails.application.secrets.mandrill_api_key, authentication: :plain }
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'localhost', port: 5000 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
