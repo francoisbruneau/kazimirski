bce764514310eddedf8bfc964adfb65fb1662f63
François Bruneau <francois.bruneau@icloud.com>
2015-06-07 17:50:38 +0200

    Created Dashboard route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  authenticated :user do
+    root :to => "welcome#dashboard", :as => "authenticated_root"
+  end
+
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
