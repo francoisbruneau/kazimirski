ca35466019f9f6fe1305dce83226f334a54dac18
François Bruneau <francois.bruneau@liip.ch>
2015-09-15 17:52:53 +0200

    Temporarily use test users on production (Heroku alpha test)

--- db/seeds/all.rb
+++ db/seeds/all.rb
@@ -9,3 +9,17 @@
 ['transcriber', 'reviewer', 'admin'].each do |role|
   Role.find_or_create_by({name: role})
 end
+
+User.create(:email => 'admin@kazimirski.fr', :password => 'password', :password_confirmation => 'password', :role => Role.find_by_name('admin'), :confirmed_at => DateTime.now)
+User.create(:email => 'reviewer@kazimirski.fr', :password => 'password', :password_confirmation => 'password', :role => Role.find_by_name('reviewer'), :confirmed_at => DateTime.now)
+User.create(:email => 'transcriber@kazimirski.fr', :password => 'password', :password_confirmation => 'password', :role => Role.find_by_name('transcriber'), :confirmed_at => DateTime.now)
+
+# Create sample pages from sample scans
+Dir.glob(Rails.root.join('public', 'scans', '*')).each do |filepath|
+  p = Page.new
+  p.scanned_image = File.open(filepath)
+  captures = filepath.match(/t(\d+)p(\d+).png/).captures
+  p.book_nr = captures.first.to_i
+  p.source_page_nr = captures.second.to_i
+  p.save
+end
